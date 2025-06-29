4cb68ed250a5d2d8acf2798333ab2d3dae55eaf6
Nguyễn Lê Trần Minh <contact.nguyenletranminh@gmail.com>
2025-06-29 14:03:30 +0700

    nltmdev

---
+++ assets/js/data.js
@@ -0,0 +1,104 @@
+const members = {
+    'nguyenletranminh': {
+        name: 'Nguyễn Lê Trần Minh',
+        image: 'assets/img/nguyenletranminh.jpg',
+        totalProjects: 15,
+        leaderProjects: 10,
+        projects: [
+            {
+                id: 1,
+                title: 'Phim ngắn học đường "Vũ Hạ"',
+                description: 'Một bộ phim ngắn do Nguyễn Lê Trần Minh làm đạo diễn hình ảnh - Biên kịch Đỗ Yến Nhi',
+                image: 'assets/img/project/vu-ha.jpg',
+                media: 'project/vu-ha.mp4'
+            },
+            {
+                id: 2,
+                title: 'Phim ngắn "Đứa Con Nghịch Tử"',
+                description: 'Một bộ phim ngắn do trưởng nhóm Nguyễn Lê Trần Minh phối hợp với Đội truyền thông trường CĐCN Huế sản xuất.',
+                image: 'assets/img/project/dua-con-nghich-tu.jpg',
+                media: 'project/dua-con-nghich-tu.jpg'
+            },
+            {
+                id: 3,
+                title: 'Tuyển sinh 2025',
+                description: 'Clip tuyển sinh do Nguyễn Lê Trần Minh làm đạo diễn hình ảnh',
+                image: 'assets/img/project/tuyen-sinh-2025.png',
+                media: 'project/video-tot-nghiep-2025.mp4'
+            },
+            {
+                id: 4,
+                title: 'Phim ngắn "Cha già"',
+                description: 'Một bộ phim ngắn do Nguyễn Lê Trần Minh làm diễn viên - đạo diễn hình ảnh.',
+                image: 'assets/img/project/cha-gia.jpg',
+                media: 'project/cha-gia-trailer.mp4'
+            },
+            {
+                id: 5,
+                title: 'Sản phẩm 1',
+                description: 'Một đoạn video với nội dung "Thanh xuân của tôi ở HueIC"',
+                image: 'assets/img/project/sanpham1.jpg',
+                media: 'project/sanpham1.mp4'
+            }
+        ]
+    },
+    'dinhphuminh': {
+        name: 'Đinh Phú Minh',
+        image: 'assets/img/dinhphuminh.jpg',
+        totalProjects: 8,
+        leaderProjects: 3,
+        projects: [
+            {
+                id: 6,
+                title: 'Chụp ảnh sự kiện 2024',
+                description: 'Bộ ảnh sự kiện do Đinh Phú Minh thực hiện.',
+                image: 'assets/img/project/su-kien-2024.jpg',
+                media: 'project/su-kien-2024.jpg'
+            },
+            {
+                id: 7,
+                title: 'Video quảng cáo sản phẩm',
+                description: 'Video quảng cáo do Đinh Phú Minh tham gia sản xuất.',
+                image: 'assets/img/project/quang-cao.jpg',
+                media: 'project/quang-cao.mp4'
+            }
+        ]
+    },
+    'buiquoctrung': {
+        name: 'Bùi Quốc Trung',
+        image: 'assets/img/buiquoctrung.jpg',
+        totalProjects: 5,
+        leaderProjects: 2,
+        projects: [
+            {
+                id: 8,
+                title: 'Thiết kế poster sự kiện',
+                description: 'Poster sự kiện do Bùi Quốc Trung thiết kế.',
+                image: 'assets/img/project/poster-su-kien.jpg',
+                media: 'project/poster-su-kien.jpg'
+            }
+        ]
+    },
+    'tranquangnhat': {
+        name: 'Trần Quang Nhật',
+        image: 'assets/img/tranquangnhat.jpg',
+        totalProjects: 7,
+        leaderProjects: 4,
+        projects: [
+            {
+                id: 9,
+                title: 'Video khám phá du lịch',
+                description: 'Video du lịch do Trần Quang Nhật thực hiện.',
+                image: 'assets/img/project/du-lich.jpg',
+                media: 'project/du-lich.mp4'
+            },
+            {
+                id: 10,
+                title: 'Phim tài liệu ngắn',
+                description: 'Phim tài liệu do Trần Quang Nhật đạo diễn.',
+                image: 'assets/img/project/tai-lieu.jpg',
+                media: 'project/tai-lieu.mp4'
+            }
+        ]
+    }
+};
